22ccbfcdd110dc93c0e3e4ad7ea10e3e86017c55
Ahmed Issawi <ahmedaswai@gmail.com>
2017-12-14 22:45:53 +0200

    Second Commit Year

---
+++ src/com/car/fayoum/utils/GeneralUtils.java
@@ -0,0 +1,13 @@
+package com.car.fayoum.utils;
+
+import java.util.BitSet;
+
+public class GeneralUtils {
+    public static BitSet bitsSet(long num) {
+        BitSet bitSet = new BitSet();
+        for (int i = 0; i < 32; i++)
+            if (((num >>> i) & 1) != 0)
+                bitSet.set(i);
+        return bitSet;
+    }
+}
